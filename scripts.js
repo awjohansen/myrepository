4b0cf9aac0b5f586c0324de4315050143961d8e8
Adam Johansen <Goblin@Adams-MacBook-Pro.local>
2016-10-21 17:49:10 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,112 @@
+var name = prompt ("What's your name?");
+
+if (name!=null){
+alert(x="Welcome to my house " + name + "!");
+alert("Let me take you on a tour");
+}
+
+alert("On the left you can see the bathroom, my brother samuel is currently in there");
+var answerOne = prompt("What do you do? Type [1] To open restroom door, Type [2] To yell Samuels name, Type [3] To continue the tour");
+
+   if(answerOne==="1"){
+   alert("You see my brother completely naked wearing scooba gear fully submerged in the bathtub. As he rises and sees you staring he screams in shock horror");
+   var answerTwo = prompt("What do you do? Type [1] To scream, close the door and continue on the tour, Type [2] To disrobe and join samuel in his scooba adventure");
+}
+        if(answerTwo==="1"){
+        alert("Samuel becomes enraged, turns green and smashes his surroundings");
+        alert("HULK SMASH")
+        alert("He's lowkey the incredible hulk, you've done ducked up");
+        var answerThree = prompt("What do you do? Type [1] To Run Away, Type [2] To compliment his biceps");
+}
+        else if(answerTwo==="2"){
+        alert("Samuel takes you on an adventure in his bathtub.")
+        alert("He teaches you how to talk to fish")
+        alert("You befriend a Mermaid")
+        alert("She teaches you her ways")
+        alert("She asks if you would like to fully commit and become a Mermaid")
+        var answerFour = prompt("What do you do? Type [1] To commit and become a Mermaid, Type [2] To not commit and live life as an insufferable mortal being");
+}
+      if(answerThree==="1"){
+        alert("You run but to no avail");
+        alert("Enraged Samuel grabs you by the skull and swings you around")
+        alert("Your head detaches from your body")
+        self.location = "https://i.ytimg.com/vi/_xx_BbkTnSY/maxresdefault.jpg"
+}
+        else if(answerThree==="2"){
+          alert("You compliment on his biceps");
+          alert("Samuel becomes dosile")
+          alert("His anger subsides")
+          alert("Hes been working out, and you're the first to notice")
+          alert("He thanks you")
+          var answerSix = prompt("A bit shook, What do you want to do? Type [1] To continue on the tour, Type [2] To disrobe and join Samuel in his scooba adventure");
+}
+        if(answerFour==="1"){
+        alert("Through an extremely painful transformation your lower body has turned into the tail of a fish");
+        alert("You live the rest of your days as a Mermaid in Samuels bathtub")
+        self.location = "http://www.happyvideonetwork.com/wp-content/uploads/2016/07/littlemermaid.jpg"
+}
+        else if(answerFour==="2"){
+        alert("The Mermaid pulls out a knife and dismembers your insides")
+        alert("Your lifeless body descends deeper into the abyss which is Samuels bathtub")
+        self.location = "http://zidbits.com/wp-content/uploads/2010/11/drown.jpg"
+}
+
+   else if(answerOne==="2"){
+   alert("You yell SAMUEL!");
+   alert("Samuel becomes enraged and smashes down the door");
+   alert("He's lowkey the incredible hulk, you've done ducked up");
+   var answerFive = prompt("What do you do? Type [1] To run away, Type [2] To compliment his biceps");
+}
+   if(answerFive==="1"){
+   alert("You run but to no avail");
+   alert("Enraged Samuel grabs you by the skull and swings you around")
+   alert("Your head detaches from your body")
+   self.location = "https://i.ytimg.com/vi/_xx_BbkTnSY/maxresdefault.jpg"
+}
+   else if(answerFive==="2"){
+   alert("You compliment on his biceps");
+   alert("Samuel becomes dosile")
+   alert("His anger subsides")
+   alert("Hes been working out, and you're the first to notice")
+   alert("He thanks you")
+   var answerSix = prompt("A bit shook, What do you want to do? Type [1] To continue on the tour, Type [2] To disrobe and join Samuel in his scooba adventure");
+}
+
+   if(answerSix==="1"){
+   alert("You contunue on the tour. We head down the hall, on the right you see a mirror but you don't see yourself");
+   alert("You question your existence");
+   alert("You wake up screaming");
+   alert("Oh what a relief. It was all a dream")
+   alert("You get out of bed and open your door to head to the bathroom to splash some water on your face")
+   alert("You open your door...")
+   alert("You're shocked at what's waiting on the other side")
+   location.reload();
+ }
+ else if(answerSix==="2"){
+ alert("Samuel takes you on an adventure in his bathtub.")
+ alert("He teaches you how to talk to fish")
+ alert("You befriend a Mermaid")
+ alert("She teaches you her ways")
+ alert("She asks if you would like to fully commit and become a Mermaid")
+ var answerEight = prompt("What do you do? Type [1] To commit and become a Mermaid, Type [2] To not commit and live life as an insufferable mortal being");
+}
+if(answerEight==="1"){
+alert("Through an extremely painful transformation your lower body has turned into the tail of a fish");
+alert("You live the rest of your days as a Mermaid in Samuels bathtub")
+self.location = "http://www.happyvideonetwork.com/wp-content/uploads/2016/07/littlemermaid.jpg"
+}
+else if(answerEight==="2"){
+alert("The Mermaid pulls out a knife and dismembers your insides")
+alert("Your lifeless body descends deeper into the abyss which is Samuels bathtub")
+self.location = "http://zidbits.com/wp-content/uploads/2010/11/drown.jpg"
+}
+else if(answerOne==="3"){
+alert("You contunue on the tour. We head down the hall, on the right you see a mirror but you don't see yourself");
+alert("You question your existence");
+alert("You wake up screaming");
+alert("Oh what a relief. It was all a dream")
+alert("You get out of bed and open your door to head to the bathroom to splash some water on your face")
+alert("You open your door...")
+alert("You're shocked at what's waiting on the other side")
+location.reload();
+}
